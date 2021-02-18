e8690672630598b8c895b081f62ea573151ece73
gmein <gmein@ucsc.edu>
2021-02-18 13:49:21 -0800

    idk

--- birthday-backend/birthdays_api/settings.py
+++ birthday-backend/birthdays_api/settings.py
@@ -110,7 +110,6 @@ LOGIN_URL='/admin/login/'
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
